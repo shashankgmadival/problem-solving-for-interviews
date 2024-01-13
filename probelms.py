9aebe9d651ce0d68097d58771a1687dfa5ca4817
shashank <shashankgm10@gmail.com>
2024-01-13 14:20:17 +0530

    5 prblms solved

---
+++ probelms.py
@@ -0,0 +1,40 @@
+# write a program to display a persons name,age and
+#address in three different lines 
+
+name='shashank '
+age=36
+address='INDIA'
+print(name)
+print (age)
+print (address)
+
+#detailed method
+
+name='shashank '
+age=36
+address='INDIA'
+print('name of the person',name)
+print ('age of the person',age)
+print ('address of the person',address)
+
+# program to swap 2 variable
+a= 10
+b= 20
+a,b=b,a
+print('value of a is',a)
+print('value of b is',b)
+
+# convert a float into int
+
+a=10.6
+print('type of a is ',type(a))
+a=(int(a))
+print('type of a is',type(a))
+
+# take a detail from students
+
+name=input('enter your name ')
+age= input(int('age of student '))
+grade=input('enter you grade')
+print(name,age,grade)
+
