c91c29a8197f669174a7f153b13ee1c0159c66ae
shashank <shashankgm10@gmail.com>
2024-02-22 00:33:59 +0530

    assignment problems

---
+++ assignment-problmes.py
@@ -0,0 +1,23 @@
+# Write a Python program to display the current date and time
+
+import datetime
+
+today=datetime.datetime.now()
+today
+
+# Solve the expression. 5.2 + True + bool(-5) - 4 + 42//5 +bool(0) + False + int(5.9) + float('1')
+
+s= 5.2 + True + bool(-5) - 4 + 42//5 +bool(0) + False + int(5.9) + float('1')
+s
+
+
+# Write code to test whether the given number lies between 1 and 10
+
+
+for i in range(1, 10):
+    print(i)
+
+    if '5' in str(i):
+        print('Yes')
+    else:
+        print('No')
